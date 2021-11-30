7767003374fcec2dd728c34e191890f5e187c68a
vupv <vupv@bap.jp>
2021-12-01 00:33:18 +0700

    update code

--- src/main/java/pvars/springsecurity/controllers/HomeController.java
+++ src/main/java/pvars/springsecurity/controllers/HomeController.java
@@ -4,17 +4,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import pvars.springsecurity.models.form.LoginForm;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 
 @Controller
 public class HomeController implements WebMvcConfigurer {
@@ -34,17 +29,7 @@ public class HomeController implements WebMvcConfigurer {
     }
 
     @GetMapping("/login")
-    public String login(LoginForm loginForm) {
-        return "login";
-    }
-
-    @PostMapping("/handle")
-    public String handleLogin(Model model, @Valid LoginForm loginForm, BindingResult bindingResult){
-        if (bindingResult.hasErrors()){
-            return "redirect:/login";
-        }
-
-        model.addAttribute("user", loginForm.getUsername());
+    public String login() {
         return "login";
     }
 
